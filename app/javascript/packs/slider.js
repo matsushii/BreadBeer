ec51382b1dc835b68818011eb01131d7224e602e
matsushii <mtsmath24@yahoo.co.jp>
2020-08-02 14:37:42 +0900

    slickを用いたスライダー（カルーセル）の導入
    Slickを用いでCDN形式でのSlider導入を行なった

---
+++ app/javascript/packs/slider.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function(){
+  $('.main__slider--image').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    fade: true,
+    allow: false,
+  });
+});
